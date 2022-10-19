e27343368cf78656bb76f13aa5ccab74c41487fb
stephbsb <stephany0303@gmail.com>
2022-10-19 16:24:59 -0400

    fix: fixed formatting

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSoma(t *testing.T) {
 	total := Sum(15, 15)
 
 	if total != 30 {
-		t.Error("Invalid result. Result: %d, Expected: %d", total, 30)
+		t.Errorf("Invalid result. Result: %d, Expected: %d", total, 30)
 	}
 }
